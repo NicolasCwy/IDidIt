fe5ddde5857624c38c74ef40a85656d5a2bb77ad
nicolascwy <25302138+NicolasCwy@users.noreply.github.com>
2024-08-10 18:01:13 +0800

    Refactor main loop to choose action

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,41 @@
 import { input } from '@inquirer/prompts'
 import { addTask, taskArray } from './TaskDb'
+import prompt from 'inquirer-interactive-list-prompt'
+
+enum Operation {
+    addTask = 'add',
+    markTask = 'mark',
+}
+
+function getOperation(input: string) {
+    if (input === 'add' || input == 'mark') {
+        return input as Operation
+    }
+
+    throw new Error('Invalid Operation')
+}
 
 async function start() {
     while (true) {
-        const taskName = await input({ message: 'task name' })
+        const operationStr: string = await prompt({
+            message: 'Select an option:',
+            choices: [
+                { name: 'Add Task', value: 'add', key: 'a' },
+                { name: 'Mark task as complete', value: 'mark', key: 'm' },
+            ],
+        })
 
-        addTask(taskName)
-        console.log(taskArray.getTasks())
+        const operation = getOperation(operationStr)
+
+        if (operation === Operation.addTask) {
+            const taskName = await input({ message: 'task name' })
+            addTask(taskName)
+            console.log(taskArray.getTasks())
+        }
+
+        if (operation === Operation.markTask) {
+            console.log('marking task')
+        }
     }
 }
 
