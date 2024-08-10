f192ca94b5d606ff4e45000f5057ec78e71ee166
nicolascwy <25302138+NicolasCwy@users.noreply.github.com>
2024-08-10 16:46:45 +0800

    Refactor and add TaskArray class

--- src/TaskDb/index.ts
+++ src/TaskDb/index.ts
@@ -1,10 +1,36 @@
 type Task = {
+    id: string
     name: string
 }
 
-export const tasks: Task[] = []
+type Id = string
+
+type Tasks = Record<Id, Task>
+
+class TaskArray {
+    tasks: Tasks = {}
+    constructor() {}
+
+    createTask(name: string) {
+        return { id: crypto.randomUUID(), name }
+    }
+
+    addTask(task: Task) {
+        const id = task.id
+        if (id in this.tasks) {
+            throw new Error('Task exists')
+        }
+        this.tasks[id] = task
+    }
+
+    getTasks(): Task[] {
+        return Object.values(this.tasks)
+    }
+}
+
+export const taskArray = new TaskArray()
 
 export function addTask(name: string) {
-    const newTask: Task = { name }
-    tasks.push(newTask)
+    const newTask: Task = taskArray.createTask(name)
+    taskArray.addTask(newTask)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { input } from '@inquirer/prompts'
-import { addTask, tasks } from './TaskDb'
+import { addTask, taskArray } from './TaskDb'
 
 async function start() {
     while (true) {
         const taskName = await input({ message: 'task name' })
 
         addTask(taskName)
-        console.log(tasks)
+        console.log(taskArray.getTasks())
     }
 }
 
